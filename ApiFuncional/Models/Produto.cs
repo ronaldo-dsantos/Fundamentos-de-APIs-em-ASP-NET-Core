cf2eb8b3607f43748414f31c970edf873541786a
ronaldo <ronaldo.dsantos@gmail.com>
2024-12-12 21:47:26 -0300

    add manipulando operacoes no banco de dados

---
+++ ApiFuncional/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        public string? Descricao { get; set; }
+    }
+}
+
